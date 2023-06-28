c79b67483a8092b27685f4a6b96f913536fba72c
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-06-28 17:57:39 -0300

    Log actions

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -8,6 +8,11 @@ export default {
     tags: ['autodocs'],
     args: {
         children: 'Send'
+    },
+    argTypes: {
+        onClick: {
+            action: 'click'
+        }
     }
 } as Meta<ButtonProps>
 
